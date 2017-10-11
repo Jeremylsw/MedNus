185884ab769ba7a8a05756a76c66c90d540fa765
Jeremy Long Shi Wei <E0014824@u.nus.edu>
2017-10-12 00:45:37 +0800

    Changed Literals Positions in comparator

--- src/test/java/seedu/address/commons/core/MessageTest.java
+++ src/test/java/seedu/address/commons/core/MessageTest.java
@@ -10,9 +10,9 @@ public class MessageTest {
     public void testMessageClass(){
         Messages messageClass = new Messages();
 
-        assertTrue(messageClass.MESSAGE_UNKNOWN_COMMAND.equals("Unknown command"));
-        assertTrue(messageClass.MESSAGE_INVALID_COMMAND_FORMAT.equals("Invalid command format! \n%1$s"));
-        assertTrue(messageClass.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX.equals("The person index provided is invalid"));
-        assertTrue(messageClass.MESSAGE_PERSONS_LISTED_OVERVIEW.equals("%1$d persons listed!"));
+        assertTrue("Unknown command".equals(messageClass.MESSAGE_UNKNOWN_COMMAND));
+        assertTrue("Invalid command format! \n%1$s".equals(messageClass.MESSAGE_INVALID_COMMAND_FORMAT));
+        assertTrue("The person index provided is invalid".equals(messageClass.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX));
+        assertTrue("%1$d persons listed!".equals(messageClass.MESSAGE_PERSONS_LISTED_OVERVIEW));
     }
 }
